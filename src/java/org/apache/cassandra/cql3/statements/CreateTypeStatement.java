05d8c17d9a466d6eba35fdddd5842d6b366fd194
Sylvain Lebresne <sylvain@datastax.com>
2013-09-20 15:28:47 +0200

    Add ALTER and DROP for types

--- src/java/org/apache/cassandra/cql3/statements/CreateTypeStatement.java
+++ src/java/org/apache/cassandra/cql3/statements/CreateTypeStatement.java
@@ -24,6 +24,7 @@ import org.apache.cassandra.auth.Permission;
 import org.apache.cassandra.config.*;
 import org.apache.cassandra.cql3.*;
 import org.apache.cassandra.db.marshal.AbstractType;
+import org.apache.cassandra.db.marshal.UTF8Type;
 import org.apache.cassandra.db.marshal.UserType;
 import org.apache.cassandra.exceptions.*;
 import org.apache.cassandra.service.ClientState;
@@ -58,13 +59,28 @@ public class CreateTypeStatement extends SchemaAlteringStatement
 
     public void validate(ClientState state) throws RequestValidationException
     {
-        UserType old = Schema.instance.userTypes.getType(name.key);
+        UserType old = Schema.instance.userTypes.getType(name);
         if (old != null)
         {
             if (ifNotExists)
                 return;
             else
                 throw new InvalidRequestException(String.format("A user type of name %s already exists.", name));
+        }
+    }
+
+    public static void checkForDuplicateNames(UserType type) throws InvalidRequestException
+    {
+        for (int i = 0; i < type.types.size() - 1; i++)
+        {
+            ByteBuffer fieldName = type.columnNames.get(i);
+            for (int j = i+1; j < type.types.size(); j++)
+            {
+                if (fieldName.equals(type.columnNames.get(j)))
+                    throw new InvalidRequestException(String.format("Duplicate field name %s in type %s",
+                                                                    UTF8Type.instance.getString(fieldName),
+                                                                    UTF8Type.instance.getString(type.name)));
+            }
         }
     }
 
@@ -76,8 +92,8 @@ public class CreateTypeStatement extends SchemaAlteringStatement
     @Override
     public String keyspace()
     {
-        // Not really a legit keyspace name, but SchemaAlteringStatement use that for schema change notification so
-        // leave that for now; TODO: we need to change that
+        // Kind of ugly, but SchemaAlteringStatement uses that for notifying change, and an empty keyspace
+        // there kind of make sense
         return "";
     }
 
@@ -85,21 +101,23 @@ public class CreateTypeStatement extends SchemaAlteringStatement
     {
         List<ByteBuffer> names = new ArrayList<>(columnNames.size());
         for (ColumnIdentifier name : columnNames)
-            names.add(name.key);
+            names.add(name.bytes);
 
         List<AbstractType<?>> types = new ArrayList<>(columnTypes.size());
         for (CQL3Type type : columnTypes)
             types.add(type.getType());
 
-        return new UserType(name.key, names, types);
+        return new UserType(name.bytes, names, types);
     }
 
     public void announceMigration() throws InvalidRequestException, ConfigurationException
     {
         // Can happen with ifNotExists
-        if (Schema.instance.userTypes.getType(name.key) != null)
+        if (Schema.instance.userTypes.getType(name) != null)
             return;
 
-        MigrationManager.announceNewType(createType());
+        UserType type = createType();
+        checkForDuplicateNames(type);
+        MigrationManager.announceNewType(type);
     }
 }
